a9700919933b9d16053cfd4db733abbd6749f204
Rahamat Jahan <rahamatjahan92@gmail.com>
2018-12-17 19:45:10 +0600

    wrote run command for migration

---
+++ src/Console/Migrate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Foundation\Console;
+
+class Migrate {
+    public function execute() {
+        echo "migrate";
+        return true;
+    }
+}
---
+++ src/Console/Run.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Foundation\Console;
+
+class Run {
+
+    protected $commandsWhitelist = [
+        'migrate' => 'Foundation\Console\Migrate'
+    ];
+
+    public function execute($args) {
+
+        if(count($args) > 1) {
+            $executable = array_shift($args);
+            $commandName = array_shift($args);
+
+            if(array_key_exists($commandName, $this->commandsWhitelist)) {
+                return $this->executeCommand($this->commandsWhitelist[$commandName], $args);
+            }
+            echo "inside if";
+            return $this->outputHelp();
+        }
+        echo "outside if";
+        return $this->outputHelp();
+    }
+
+    protected function outputHelp() {
+        echo 'Available commands:' . PHP_EOL;
+        foreach($this->commandsWhitelist as $key => $value) {
+            echo 'php run ' . $key . PHP_EOL;
+        }
+
+        return false;
+    }
+
+    protected function executeCommand($commandClass, $args) {
+        $command = new $commandClass;
+        return $command->execute(...$args);
+    }
+}
