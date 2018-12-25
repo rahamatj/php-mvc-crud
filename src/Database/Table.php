2eca4a3270f6cb61a9eab0bbcf81e58a854ed111
Rahamat Jahan <rahamatjahan92@gmail.com>
2018-12-25 10:36:40 +0600

    unique function added in Table

--- src/Database/Table.php
+++ src/Database/Table.php
@@ -19,21 +19,25 @@ class Table {
     public function increments($fieldName) {
         $this->fields[] 
             = "{$fieldName} int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT";
-        return $this;
+
+            return $this;
     }
 
     public function integer($fieldName) {
         $this->fields[] = "{$fieldName} int(10) NOT NULL";
+
         return $this;
     }
 
     public function tinyInteger($fieldName) {
         $this->fields[] = "{$fieldName} tinyint(1) NOT NULL";
+
         return $this;
     }
 
     public function decimal($fieldName, $m = 10, $d = 2) {
         $this->fields[] = "{$fieldName} DECIMAL({$m}, ${d}) NOT NULL";
+
         return $this;
     }
 
@@ -43,49 +47,65 @@ class Table {
         }, $values);
         $this->fields[] 
             = "{$fieldName} ENUM(" . implode(',',$values) . ") NOT NULL";
+
         return $this;
     }
 
     public function string($fieldName) {
         $this->fields[] = "{$fieldName} varchar(191) NOT NULL";
+
         return $this;
     }
 
     public function text($fieldName) {
         $this->fields[] = "{$fieldName} text NOT NULL";
+
         return $this;
     }
 
     public function boolean($fieldName) {
         $this->fields[] = "{$fieldName} tinyint(1) NOT NULL";
+
         return $this;
     }
 
     public function date($fieldName) {
         $this->fields[] = "{$fieldName} DATE NOT NULL";
+
         return $this;
     }
 
     public function dateTime($fieldName) {
         $this->fields[] = "{$fieldName} DATETIME NOT NULL";
+
         return $this;
     }
 
     public function timestamp($fieldName) {
         $this->fields[] 
             = "{$fieldName} TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP";
+
         return $this;
     }
 
     public function nullable() {
         $this->fields[] 
             = str_replace("NOT", "DEFAULT", array_pop($this->fields));
-        return $this;
+        
+            return $this;
     }
 
     public function default($value) {
         $this->fields[] 
             = array_pop($this->fields) . " DEFAULT '" . $value . "'";
+        
+            return $this;
+    }
+
+    public function unique() {
+        $this->fields[]
+            = array_pop($this->fields) . " UNIQUE";
+        
         return $this;
     }
 
